9754f0612d2b1666c5b318f694867876b81d5994
amrita-c-web <54478846+amrita-c-web@users.noreply.github.com>
2021-09-01 21:08:48 +0545

    complete home page

--- src/components/Adminstration.jsx
+++ src/components/Adminstration.jsx
@@ -3,9 +3,38 @@ import React from 'react'
 export default function Adminstration() {
     return (
         <>
-        <div className="row">
-        <h1 className="text-center" style={{ backgroundColor: '#2CD62C', border: "none" }}>Administrative</h1>
-            <div className="adminstration-section col-12">
+            <div className="row">
+                <h1 className="text-center" style={{ backgroundColor: '#2CD62C', border: "none" }}>Administrative</h1>
+                <div class="container">
+                    <div class="row row-cols-2  g-2 g-lg-3">
+                        <div className="col">
+                            <div class="row row-cols-1 row-cols-md-2">
+                                <div className="col">
+                                    <img src="https://picsum.photos/200" className="img-fluid rounded-start p-1" alt="profileimage" />
+                                </div>
+                                <div className="col">
+                                    <h5 className="card-title">Manager</h5>
+                                    <p className="card-text">Amrita Shrestha</p>
+                                    <p className="card-text">98XXXXXXXXXX</p>
+                                    <p className="card-text"><small className="text-muted">In-service since 2070</small></p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col">
+                            <div class="row row-cols-1 row-cols-md-2">
+                                <div className="col">
+                                    <img src="https://picsum.photos/200" className="img-fluid rounded-start p-1" alt="profileimage" />
+                                </div>
+                                <div className="col">
+                                <h5 className="card-title">CEO</h5>
+                                        <p className="card-text">Amrita Shrestha</p>
+                                        <p className="card-text">98XXXXXXXXXX</p>
+                                        <p className="card-text"><small className="text-muted">In-service since 2070</small></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {/* <div className="adminstration-section col-12">
                 <div className="row">
                 <div className="col-6 d-flex">
                     <div className="">
@@ -33,11 +62,11 @@ export default function Adminstration() {
                 </div>
                 </div>
                 
-            </div>
-            
+            </div> */}
 
-        </div>
-            {/* <div className="row">
+
+                </div>
+                {/* <div className="row">
                 <div className="service mt-2">
                     <h1 className="card text-center" style={{ backgroundColor: '#2CD62C', border: "none" }}>Administrative</h1>
                 </div>
@@ -74,6 +103,7 @@ export default function Adminstration() {
                     </div>
                 </div>
             </div> */}
+            </div>
         </>
     )
 }
